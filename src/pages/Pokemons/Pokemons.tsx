68279ef499af691f873a2dca12a256240349a641
abr-ya <gamer05@list.ru>
2024-07-16 08:25:00 +0300

    build: Add vite-plugin-federation and config it

--- src/pages/Pokemons/Pokemons.tsx
+++ src/pages/Pokemons/Pokemons.tsx
@@ -1,5 +1,11 @@
-const Pokemons = () => {
-  return <div>Pokemons</div>;
-};
+// @ts-expect-error todo: mf types!
+import PokemonList from "remote1/PokemonList";
+
+const Pokemons = () => (
+  <div>
+    Pokemons from {import.meta.env.VITE_REM1}
+    <PokemonList />
+  </div>
+);
 
 export default Pokemons;
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,33 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import federation from "@originjs/vite-plugin-federation";
 import { resolve } from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: [{ find: "@", replacement: resolve(__dirname, "src") }],
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  const API_REM1 = `${env.VITE_REM1 ?? "http://localhost:5173/"}`;
+
+  return {
+    plugins: [
+      react(),
+      federation({
+        name: "pokemonHome",
+        remotes: {
+          remote1: `${API_REM1}assets/remoteEntry.js`,
+        },
+        shared: ["react", "react-dom"],
+      }),
+    ],
+    build: {
+      modulePreload: false,
+      target: "esnext",
+      minify: false,
+      cssCodeSplit: false,
+    },
+    resolve: {
+      alias: [{ find: "@", replacement: resolve(__dirname, "src") }],
+    },
+  };
 });
